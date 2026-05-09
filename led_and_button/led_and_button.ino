fd6e7fb850ceb2d319ce64f467a9a8e7dd88af46
Troyanok <troyanok.tanya@mail.ru>
2026-05-09 17:59:03 +0300

    Fix: names

---
+++ led_and_button/led_and_button.ino
@@ -0,0 +1,20 @@
+const int LED=9; // Присваиваем контакту 9 идентификатор LED
+const int BUTTON=2; // Присваиваем контакту 2 идентификатор BUTTON
+
+void setup()
+{
+  pinMode(LED,OUTPUT); // Задаём для контакта LED режим вывода
+  pinMode(BUTTON,INPUT); // Задаём для контакта LED режим ввода
+}
+
+void loop()
+{
+  if (digitalRead(BUTTON) == LOW)
+  {
+    digitalWrite(LED, LOW);
+  }
+  else
+  {
+    digitalWrite(LED, HIGH);
+  }
+}
